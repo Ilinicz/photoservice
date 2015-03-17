af7c012c01809e4b1d36d10b69802ef65979ae61
Vincent Ilinicz <vilinicz@mail.ru>
2015-03-17 03:49:41 +0300

    sass fix 2

--- config/initializers/compression.rb
+++ config/initializers/compression.rb
@@ -22,9 +22,9 @@ Rails.application.configure do
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 
   config.middleware.use HtmlCompressor::Rack,
-    compress_css: true,
+    #compress_css: true,
     compress_javascript: true,
-    css_compressor: Sass,
+    #css_compressor: Sass,
     enabled: true,
     javascript_compressor: uglifier,
     preserve_line_breaks: false,
