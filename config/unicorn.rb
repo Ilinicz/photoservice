2fd6075d198cae66035b96160d5a600c744b2a11
Vincent Ilinicz <vilinicz@mail.ru>
2015-03-22 20:10:23 +0300

    unicorn conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 130
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
@@ -11,6 +11,12 @@ before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
     Rails.logger.info('Disconnected from ActiveRecord')
+    
+  defined?(Resque) and
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
+  end
+
 end
 
 after_fork do |server, worker|
